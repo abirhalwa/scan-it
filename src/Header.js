ac5ed09fd48a26bdb03dcc16c2487004409692bd
abirhalwa <abir.halwa@gmail.com>
2026-03-27 08:39:43 -0400

    Fix Node 24 build and logo asset path

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
 // this function component is used to load the header in the page
 function Header(props){
+    const logoSrc = `${process.env.PUBLIC_URL}/assets/logo.png`;
+    const homeHref = process.env.PUBLIC_URL ? `${process.env.PUBLIC_URL}/` : '/';
+
     return (
         <header className="wrapper">
-            <a href='index.html'>
-                <img src="./assets/logo.png" alt="scan it logo"/>
+            <a href={homeHref}>
+                <img src={logoSrc} alt="scan it logo"/>
             </a>
             <i aria-hidden="true" className="fas fa-info-circle"  onClick={() => props.showInfo()}>
                 <span className="srOnly">click to show help information</span>
